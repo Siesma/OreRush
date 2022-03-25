f856e917e8ce2cf161a4aea4cb3ebf522be8c6e1
sebastien <s.muheim@stud.unibas.ch>
2022-03-25 21:57:57 +0100

    Commented out several commandline outputs from pingThread (used for testing)

--- src/main/java/game/server/PingThread.java
+++ src/main/java/game/server/PingThread.java
@@ -11,7 +11,7 @@ public class PingThread implements Runnable {
         System.out.println("Ping thread started");
         while (true) {
             for(ClientThread clientThread :Server.getClientThreads()) {
-                System.out.println("Ping sent");
+                // System.out.println("Ping sent");
                 sendPing(clientThread.getOutputStream());
                 try {
                     Thread.sleep(3000);
@@ -23,7 +23,7 @@ public class PingThread implements Runnable {
                     clientsWithNoResponse.add(clientThread);
                     disconnectClient(clientThread);
                 } else {
-                    System.out.println("Ping received");
+                    // System.out.println("Ping received");
                 }
             }
 
@@ -35,7 +35,7 @@ public class PingThread implements Runnable {
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            System.out.println(Server.getClientThreads().size() +" client(s) got pinged");
+            // System.out.println(Server.getClientThreads().size() +" client(s) got pinged");
         }
 
     }
