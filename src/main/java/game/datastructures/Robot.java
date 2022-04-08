2486e72bb045cad63ad50f12988e2c8c9dcaceb1
Gian <gian.moser@stud.unibas.ch>
2022-04-09 00:52:56 +0200

    Improved the documentation of the Robot Class

--- src/main/java/game/datastructures/Robot.java
+++ src/main/java/game/datastructures/Robot.java
@@ -14,21 +14,39 @@ public class Robot {
     private Object inventory;
 
 
-
+    /**
+     * This method sets the robots position based on a given x,y value.
+     *
+     * @param x The x coordinate the robot should move to
+     * @param y The y coordinate the robot should move to
+     */
     public void setPosition(int x, int y) {
         xCoordinate = x;
         yCoordinate = y;
     }
+
+    /**
+     * This will put the given object into the inventory of the robot
+     *
+     * @param objectToLoad The object that should be loaded into the inventory of the robot
+     */
     public void loadInventory(Object objectToLoad) {
         inventory = objectToLoad;
     }
 
+    /**
+     * @return An array [x,y] of the current coordinates of the robot.
+     */
     public int[] getPosition() {
         int[] coordinate = new int[2];
         coordinate[0] = yCoordinate;
         coordinate[1] = xCoordinate;
         return coordinate;
     }
+
+    /**
+     * @return The object currently stored in the robots inventory
+     */
     public Object getInventory() {
         return inventory;
     }
