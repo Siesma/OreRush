1b50afbc5e045d3b2786768409c1d4d08e792e49
Gian <gian.moser@stud.unibas.ch>
2022-04-12 22:31:30 +0200

    Implemented "getPosition" and "setPosition" methods in the "Ore" Class. Refactored the code and moved "getPosition" and "setPosition" to the "GameObject" interface.

--- src/main/java/game/datastructures/GameObject.java
+++ src/main/java/game/datastructures/GameObject.java
@@ -3,6 +3,19 @@ package game.datastructures;
 public abstract interface GameObject {
 
     /**
+     * This method sets the GameObject position based on a given x,y value.
+     *
+     * @param x The x coordinate the GameObject should move to
+     * @param y The y coordinate the GameObject should move to
+     */
+    public void setPosition(int x, int y);
+
+    /**
+     * @return An array [x,y] of the current coordinates of the GameObject.
+     */
+    public int[] getPosition();
+
+    /**
      * This function should encode all the information of a GameObject into a string to allow data
      * transfer via a package.
      * @return The encoded string that holds all the information of the robot
