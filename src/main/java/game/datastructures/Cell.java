7a46c5f0ffed2453d3383d20413532096d6d5872
Tom-Rodewald <tom.rodewald@stud.unibas.ch>
2022-04-29 09:24:35 +0200

    Added some JavaDocs for the Cell object.

--- src/main/java/game/datastructures/Cell.java
+++ src/main/java/game/datastructures/Cell.java
@@ -15,6 +15,10 @@ public class Cell {
     placedObjects = new ArrayList<>();
   }
 
+  /**
+   *
+   * @return a list of all the different Robots that are on this cell.
+   */
   public ArrayList<Robot> robotsOnCell() {
     ArrayList<Robot> robots = new ArrayList<>();
     for (GameObject gameObject : placedObjects) {
@@ -28,6 +32,10 @@ public class Cell {
     return robots;
   }
 
+  /**
+   * This function should only find one radar anyway because if on a cell someone is digging it should remove every other radar.
+   * @return the current Radar that is present on this cell, will return null if no Radar is present.
+   */
   public Radar radarOnCell () {
     Radar out = null;
     for (GameObject gameObject : placedObjects) {
@@ -38,6 +46,11 @@ public class Cell {
     return out;
   }
 
+
+  /**
+   * This function should only find one trap anyway because if someone is digging on a cell every trap is set off.
+   * @return the current Trap that is present on this cell, will return null if no Trap is present.
+   */
   public Trap trapOnCell () {
     Trap out = null;
     for (GameObject gameObject : placedObjects) {
@@ -48,6 +61,10 @@ public class Cell {
     return out;
   }
 
+  /**
+   *
+   * @return a list of all the different Ores that are on the given cell. This can include two times the same ore.
+   */
   public ArrayList<Ore> oreOnCell () {
     ArrayList<Ore> ores = new ArrayList<>();
     for (GameObject gameObject : placedObjects) {
@@ -61,6 +78,11 @@ public class Cell {
     return ores;
   }
 
+  /**
+   * This function should only be called upon initialization and not during the actual update mechanic!
+   * This function adds the "object" (Paramter) onto this cell's ArrayList of GameObjects.
+   * @param object that is being placed onto the map
+   */
   public void place(GameObject object) {
     placedObjects.add(object);
   }
