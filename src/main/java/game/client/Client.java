68f02e78df737e58f04838ff37703946c805dd14
sebastien <s.muheim@stud.unibas.ch>
2022-05-11 17:41:18 +0200

    Added javadoc to all methods and variables of the client class to greatly improve the readability, intelligibility, accessibility and comprehensibility of the code.

--- src/main/java/game/client/Client.java
+++ src/main/java/game/client/Client.java
@@ -25,29 +25,67 @@ import java.net.Socket;
  * It holds information of the socket for server connection
  */
 public class Client {
-
+  /**
+   * Static reference to this client used as model by the GUI controllers
+   */
   private static Client client;
-
+  /**
+   * Outputstream of the client used to send packets to the server
+   */
   private static OutputStream outputStream;
+  /**
+   * Socket of this client used to connect with the server
+   */
   private Socket socket;
+  /**
+   * Inputstream of this client used to receive packages of the server
+   */
   private InputStream inputStream;
+  /**
+   * Boolean to save if a pong packet from the server was received
+   * is set to true if a pong packet was received
+   * set to false after a success packet was sent, confirming that a pong packet was received
+   */
   private boolean pongReceived = false;
-
+  /**
+   *stringproperty used to show the nickname the player chose in the GUI
+   */
   private final StringProperty nickname;
+  /**
+   * Stringproperty used to update the chat text area of the StartMenu controller.
+   * Is used by all packets updating the chat area such as chat, whisper, broadcast and highscore.
+   */
   private final StringProperty lastChatMessage = new SimpleStringProperty();
-
+  /**
+   * LobbyInClient used to store the lobby information in which the client is.
+   */
   private LobbyInClient lobbyInClient;
-
+  /**
+   * ObservableList of lobby used as model for the start menu controller allowing to see which lobby are currently
+   * on the server, their status and the players in them
+   */
   private final ObservableList<LobbyInClient> lobbyInClientObservableList = FXCollections.observableArrayList();
-
+  /**
+   * Observable list of client names that are currently connected to the server updated by the client
+   */
   ObservableList<String> observableClientList = FXCollections.observableArrayList();
+  /**
+   * List property  of the observable list of clients that allows the start menu controller to bind the list to a list view
+   */
   ListProperty<String> clientList = new SimpleListProperty<>(observableClientList);
-
-  ObservableList<String> observableLobbyList = FXCollections.observableArrayList();
-  ListProperty<String> lobbyList = new SimpleListProperty<>(observableLobbyList);
-
+  /**
+   * Log4j logger that allows great, clear and useful logging of information and errors
+   * instead of the ugly commandline prints
+   */
   public static final Logger logger = LogManager.getLogger(Client.class);
 
+  /**
+   * Constructor of the Client class. Used at the start of the GUI, used as the model of the start menu controller and
+   * lobby controller
+   * @param hostAddress address to connect to the server, can be an ip address in string form or the string "localhost".
+   * @param port integer representing the port used by the server.
+   * @param name string of the nickname of the player
+   */
   public Client(String hostAddress, int port, String name) {
     client = this;
     this.nickname = new SimpleStringProperty(name);
@@ -80,7 +118,10 @@ public class Client {
     }
   }
 
-
+  /**
+   * getter for the client
+   * @return client
+   */
   public static Client getClient() {
     return client;
   }
@@ -248,16 +289,6 @@ public class Client {
   public void removeClient(String clientName) {
     Platform.runLater(() -> observableClientList.remove(clientName));
   }
-
-  /**
-   * add the Lobby to the observableLobbyList
-   *
-   * @param lobbyName the name of the lobby that should be added to the lobby list
-   */
-  public void addLobby(String lobbyName) {
-    Platform.runLater(() -> observableLobbyList.add(lobbyName));
-  }
-
   /**
    * Sends a new ChatLobby-packet and encodes it with the predetermined content
    *
@@ -267,40 +298,68 @@ public class Client {
     lobbyInClientObservableList.add(new LobbyInClient(lobbyName));
   }
 
+  /**
+   * Getter for the outpustream
+   * @return outpustream of the client
+   */
   public OutputStream getOutputStream() {
     return outputStream;
   }
 
+  /**
+   * Getter for the inputstream
+   * @return inputstream of the client
+   */
   public InputStream getInputStream() {
     return inputStream;
   }
 
+  /**
+   * getter for the pong received boolean
+   * @return pong received boolean state
+   */
   public boolean isPongReceived() {
     return pongReceived;
   }
 
+  /**
+   * Setter for the pong received boolean
+   * @param pongReceived boolean that is new state of the pong received boolean
+   */
   public void setPongReceived(boolean pongReceived) {
     this.pongReceived = pongReceived;
   }
 
+  /**
+   * Getter for the nickname property of the client
+   * @return the nickname property of the client
+   */
   public StringProperty nicknameProperty() {
     return nickname;
   }
-
+  /**
+   * Getter for the last Chat Message Property of the client
+   * @return the last Chat Message Property of the client
+   */
   public StringProperty lastChatMessageProperty() {
     return lastChatMessage;
   }
 
+  /**
+   * Setter for the last chat message property
+   * Uses Platform.runLater to not conflict with javafx threads
+   * @param message string that is set as the last chat message property of the client
+   */
   public void setLastChatMessage(String message) {
     Platform.runLater(() -> lastChatMessage.setValue(message));
   }
 
+  /**
+   * Getter for the client list property
+   * @return the client list property
+   */
   public ListProperty<String> clientListProperty() {
     return clientList;
-  }
-
-  public ListProperty<String> lobbyListProperty() {
-    return lobbyList;
   }
 
   /**
@@ -314,32 +373,42 @@ public class Client {
     Platform.runLater(() -> observableClientList.set(observableClientList.indexOf(oldNickname), newNickname));
   }
 
+  /**
+   * Getter for the lobby in client of the client
+   * @return the lobby in client of the client
+   */
   public LobbyInClient getLobbyInClient() {
     return lobbyInClient;
   }
 
-  public void printCurrentGameMap(GameMap gameMap) {
-//    GameMap.printMapToConsole(gameMap);
-//    GameMap.printMapToConsole(gameMap.getIndividualGameMapForPlayer(nickname.get()));
-  }
-
+  /**
+   * Getter for the nickname string of the client
+   * @return the nickname string of the client
+   */
   public String getNickname() {
     return nickname.get();
   }
 
+  /**
+   * Setter for the lobby in client of the client
+   * @param lobbyInClient LobbyInClient that is the new lobbyInClient of the client
+   */
   public void setLobbyInClient(LobbyInClient lobbyInClient) {
     this.lobbyInClient = lobbyInClient;
   }
 
+  /**
+   * Getter for the ObservableList of LobbyInClient of the client
+   * @return the ObservableList of LobbyInClient of the client
+   */
   public ObservableList<LobbyInClient> getLobbyData() {
     return lobbyInClientObservableList;
   }
 
-  public ObservableList<String> getObservableLobbyList() {
-    return observableLobbyList;
-  }
-
-
+  /**
+   * Handle for the Highscore button of the start menu controller
+   * sends a packet asking for the high score list to be sent to this client
+   */
   public void sendHighScore() {
     (new PacketHandler(this)).pushMessage(outputStream, (new HighScore()).encodeWithContent());
   }
