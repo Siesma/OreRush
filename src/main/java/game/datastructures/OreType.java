710682402457e57f0a1442a6c4dd75f790794ab9
Gian <gian.moser@stud.unibas.ch>
2022-05-14 11:33:50 +0200

    Updated JavaDoc in OreType

--- src/main/java/game/datastructures/OreType.java
+++ src/main/java/game/datastructures/OreType.java
@@ -7,12 +7,23 @@ public enum OreType {
 
     Copper(1), Iron(2), Gold(4), Platinum(6), Indium(9);
 
+    /**
+     * The value of points this type of are is worth
+     */
     int value;
 
+    /**
+     * Constructor of the OreType
+     * @param value The desired value the OreType should hold
+     */
     OreType(int value) {
         this.value = value;
     }
 
+    /**
+     * Getter for the Value of the OreType
+     * @return
+     */
     public int getValue() {
         return this.value;
     }
