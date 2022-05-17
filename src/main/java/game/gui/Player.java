b2a465731d1bfaa24e428160a005f85569898507
Gian <gian.moser@stud.unibas.ch>
2022-05-17 13:39:16 +0200

    Added Javadoc to the Player Class

--- src/main/java/game/gui/Player.java
+++ src/main/java/game/gui/Player.java
@@ -9,33 +9,67 @@ import javafx.beans.property.StringProperty;
  */
 public class Player {
 
+    /**
+     * The nickname of the Player
+     */
     private final StringProperty nickname;
+    /**
+     * The score of the Player
+     */
     private StringProperty score = new SimpleStringProperty("0");
 
+    /**
+     * The constructor for the Player Class
+     * @param nickname the Nickname the player should bear
+     */
     public Player(String nickname) {
         this.nickname = new SimpleStringProperty(nickname);
     }
 
+    /**
+     * Getter for the Nickname of the player
+     * @return the nickname of the player as a String
+     */
     public String getNickname() {
         return nickname.get();
     }
 
+    /**
+     * Getter for the nickname stringProperty of the player
+     * @return the stringproperty, holding the nickname
+     */
     public StringProperty nicknameProperty() {
         return nickname;
     }
 
+    /**
+     * Setter for the Nickname of the Player
+     * @param nickname the new nickname of the Player
+     */
     public void setNickname(String nickname) {
         this.nickname.set(nickname);
     }
 
+    /**
+     * Getter for the Score of the player
+     * @return the score of the player
+     */
     public String getScore() {
         return score.get();
     }
 
+    /**
+     * Getter for the stringProperty holding the score of the player
+     * @return the stringProperty holding the score of the player
+     */
     public StringProperty scoreProperty() {
         return score;
     }
 
+    /**
+     * Setter for the score of the player
+     * @param score new score of the player
+     */
     public void setScore(String score) {
         this.score.set(score);
     }
